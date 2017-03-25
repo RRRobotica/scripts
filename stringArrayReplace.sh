ed64a77001187d873931e62bf23de33f43cbafaf
Wenfeng CAI <wenfeng.cai@demc.com.cn>
2017-03-25 17:27:17 +0800

    Use FNR instead of NR when filtering pair file

--- stringArrayReplace.sh
+++ stringArrayReplace.sh
@@ -3,7 +3,7 @@
 # Ignore first line and remove pairs with two identical words in csv file
 RCSV="{\n"`awk -F, '
 BEGIN {RS="\r|\n|\r\n|\n\r";ORS="\n"}
-NR > 1 && $1 != $2 {print "gsub(/\<" $1 "\>/,", "\"" $2 "\"" ")"}
+FNR > 1 && $1 != $2 {print "gsub(/\<" $1 "\>/,", "\"" $2 "\"" ")"}
 ' $1`"\n}1"
 
 echo "$RCSV" | gawk -f - -- $2 > $3
